aec2856c5cdef6639e603920e8881099651191ec
Igor Ferreira <142438559+IgorF3rreira@users.noreply.github.com>
2023-11-29 16:02:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Digite um número para gerar a tabuada: ");
+string numero = Console.ReadLine();
+
+int num1 = int.Parse(numero);
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("*****************");
+for (int i = 1; i < 11; i++)
+{
+    int res = num1 * i;
+
+    Console.WriteLine(num1  + " X " + i +" = " + res);
+    
+};
+Console.WriteLine("*****************");
+Console.WriteLine("");
+Console.WriteLine("//////////////////");
+Console.WriteLine("//FIM DA TABUADA//");
+Console.WriteLine("//////////////////");
